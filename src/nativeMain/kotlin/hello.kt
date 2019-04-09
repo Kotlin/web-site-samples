53c3ad2820383f3c83a7c0c8c89453b7e8112397
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-04-09 12:29:01 +0200

    complete example .def file for https://kotlinlang.org/docs/tutorials/native/mapping-function-pointers-from-c.html

--- src/nativeMain/kotlin/hello.kt
+++ src/nativeMain/kotlin/hello.kt
@@ -1,8 +1,12 @@
 import interop.*
+import kotlinx.cinterop.invoke
+import kotlinx.cinterop.staticCFunction
 
 fun main() {
   println("Hello Kotlin/Native!")
 
-  accept_fun(/*fix me */)
-  supply_fun()
+  accept_fun(staticCFunction<Int, Int> { it + 1})
+
+  val useMe = supply_fun()
+  useMe?.invoke(42)
 }
