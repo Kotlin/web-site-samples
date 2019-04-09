c5dc9c0430363c9ac603163686110289c481fbf0
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-04-09 10:22:53 +0200

    complete example for https://kotlinlang.org/docs/tutorials/native/mapping-struct-union-types-from-c.html

--- src/nativeMain/kotlin/hello.kt
+++ src/nativeMain/kotlin/hello.kt
@@ -1,11 +1,30 @@
 import interop.*
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.cValue
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.ptr
+import kotlinx.cinterop.readValue
 
 fun main() {
   println("Hello Kotlin/Native!")
 
-  struct_by_value()
-  struct_by_pointer()
-  union_by_value()
-  union_by_pointer()
-}
+  val cUnion = cValue<MyUnion> {
+    b.a = 5
+    b.b = 2.7182
+  }
 
+  memScoped {
+    union_by_value(cUnion)
+    union_by_pointer(cUnion.ptr)
+  }
+
+  memScoped {
+    val cStruct = alloc<MyStruct> {
+      a = 42
+      b = 3.14
+    }
+
+    struct_by_value(cStruct.readValue())
+    struct_by_pointer(cStruct.ptr)
+  }
+}
