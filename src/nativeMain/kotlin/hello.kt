47f74b3599aa2534a1a2ee490496c79f38984d27
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-04-09 12:52:16 +0200

    complete example for https://kotlinlang.org/docs/tutorials/native/mapping-strings-from-c.html

--- src/nativeMain/kotlin/hello.kt
+++ src/nativeMain/kotlin/hello.kt
@@ -1,12 +1,29 @@
 import interop.copy_string
 import interop.pass_string
 import interop.return_string
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.cstr
+import kotlinx.cinterop.toKString
+import kotlinx.cinterop.usePinned
 
 fun main() {
   println("Hello Kotlin/Native!")
 
-  pass_string(/*fix me*/)
-  val useMe = return_string()
-  val useMe2 = copy_string(/*fix me*/)
+  val str = "this is a Kotlin String"
+
+  pass_string(str.cstr)
+
+  val useMe = return_string()?.toKString() ?: error("null pointer returned")
+  println(useMe)
+
+  val stringFromC = ByteArray(255).usePinned { pinned ->
+
+    val useMe2 = copy_string(pinned.addressOf(0), pinned.get().size - 1)
+
+    if (useMe2 != 0) throw Error("Failed to read string from C")
+    pinned.get().stringFromUtf8()
+  }
+
+  println(stringFromC)
 }
 
